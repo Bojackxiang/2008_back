b7c5c63dbd820a1d6eabd73dacfeed14cb28f655
weijiexiang <weijiexiang@weijiexiangdeMacBook-Pro.local>
2018-06-29 16:48:49 +1000

    change envir port

--- environments.js
+++ environments.js
@@ -2,7 +2,7 @@ const date = new Date();
 
 module.exports = {
     "auther":           "weijie xiang",
-    "port":             "80",
+    "port":             process.env.PORT || "8080",
     "mongoUrl":         "mongodb://weijiexiang:911004wsrA@ds221271.mlab.com:21271/registers",
     "mongodblocal":     "mongodb://localhost:27017/Register",
     "test":             "this is a test for the following line" + date.toString(),
