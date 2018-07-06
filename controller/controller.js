abc87fc8ea18fbad4a7166030f0457b39a715f2b
Bojackxiang <batmandocode@gmail.com>
2018-07-06 21:44:10 +1000

    u

--- controller/controller.js
+++ controller/controller.js
@@ -10,7 +10,7 @@ var transporter = nodemailer.createTransport({
   port: "465",
   secure: String( process.env.EMAIL_PORT || "465") === "465" ? true: false,
   auth: {
-    user: "admin@aisfexpo.com.au",
+    usgiter: "admin@aisfexpo.com.au",
     pass: "Pg2CSxxSJCBErXUQ8JqetAmDtmcudZ6B",
   }
 });
@@ -19,8 +19,8 @@ exports.sending = function(email) {
   var mailOptions = {
     from: "admin@aisfexpo.com.au",
     to: "",
-    subject: "aisf",
-    html: "<div><p>亲爱的同学:<br> 您好，<br> 恭喜您成功注册，成为2018年第二届华为AISF澳大利亚国际留学生节专属会员，以下是你的AISF活动当日专属入场券，这将是您入场华为AISF国际留学生节活动的唯一凭证。请妥善保管！建议您保存并下载到手机，以便之后使用。<br></p><img style='width:200px;' src='https://www.aisfexpo.com.au/assets/ticket.JPG'><p>时电子使用券，登陆AISF官网：https://www.aisfexpo.com.au/，即可了解所有合作商家的促销信息，享受专属AISF会员的优惠。使用时出示本券即可。此券即ALL PASS临时电子使用券将于第二届华为AISF国际留学生节之后过期。</p><p>若想持续享受商家优惠，请于8月18日活动当日，凭此券免费入场并兑换ALL PASS实体卡。ALLPASS福利卡商家优惠将会持续更新，敬请关注www.aisfexpo.com.au查看最新优惠和福利！</p><p>感谢您对本次华为AISF澳大利亚国际留学生节的支持！ 8月18日，现场将汇聚全澳各行业超过40家企业与学校，现场为您解决留学生活中遇到的难题。再次诚挚地感谢您的支持与厚爱，8月18日SMC，我们不见不散！</p><p>2018年第二届华为AISF澳大利亚国际留学生节 时间：8月18日1pm-5pm 地点：SMC, 281 Castlereagh St, Sydney</p><p>AISF国际留学生节组委</p> </div>" // html body
+    subject: "恭喜您成功注册",
+    html: "<div style='font-size:12px;'><p>亲爱的同学,您好<br><br>恭喜您成功注册成为2018第二届华为AISF澳大利亚国际留学生节专属会员，以下是您活动专属入场券。<br><br> 这将是您入场本次活动的唯一凭证。请妥善保管！建议您长按保存图片并下载到手机，以便之后使用。<br></p><div style='text-align: center;'><img style='width:200px;' src='https://www.aisfexpo.com.au/assets/ticket.JPG'></div><p>时电子使用券，登陆AISF官网：https://www.aisfexpo.com.au/，即日起至8月18日，这张入场券将作为ALL PASS福利卡的临时电子使用券，登陆AISF官网：https://www.aisfexpo.com.au，即可了解所有合作商家的促销信息，享受专属AISF会员优惠。使用时出示本券即可，本券有效期至2018年8月18日。</p><p>若想持续享受商家优惠，请于8月18日活动当天，凭此券免费入场并兑换ALL PASS实体卡。ALL PASS福利卡商家优惠将持续更新，敬请关注www.aisfexpo.com.au</p><p>感谢您对本次华为AISF澳大利亚国际留学生节的支持！ 8月18日，我们不见不散！</p><p>2018年第二届华为AISF澳大利亚国际留学生节 时间：8月18日1pm-5pm 地点：SMC, 281 Castlereagh St, Sydney</p><p>AISF国际留学生节组委</p> <div style='text-align: center;'><img src='https://www.aisfexpo.com.au/assets/footer.JPG'></div></div>", // html body
   };
 
   mailOptions["to"] = email;
