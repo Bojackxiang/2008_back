9c776bbf5c92b2644ed3ac3765fb7bff0ed73142
Bojackxiang <batmandocode@gmail.com>
2018-07-06 11:58:17 +1000

    u

--- environments.js
+++ environments.js
@@ -3,7 +3,7 @@ const date = new Date();
 module.exports = {
     "auther":           "weijie xiang",
     "port":             process.env.PORT || "8080",
-    "mongoUrl":         "mongodb://weijiexiang:911004wsrA@ds221271.mlab.com:21271/registers",
+    "mongoUrl":         "mongodb://root_aisf_2641178338343315:2FJSfJsYA2ykTtLVHhPS5MQysZ4BmxhWGDXN7DKW2f4UAMLXpHeBWtK33uGEaWkJ@localhost:27017/aisf_db",
     "mongodblocal":     "mongodb://localhost:27017/Register",
     "test":             "this is a test for the following line" + date.toString(),
 }
